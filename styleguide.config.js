2ebf3aae3cc07613dadf1ed6a6c4bd9010f02cbc
Davi Teodoro <davi.a.teodoro@gmail.com>
2019-10-09 22:05:00 -0300

    styleguide on folder for config files

--- styleguide.config.js
+++ styleguide.config.js
@@ -22,7 +22,7 @@ module.exports = {
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
-                        options: require('./babel.config')
+                        options: require('./styleguide/babel.config')
                     }
                 },
                 {
