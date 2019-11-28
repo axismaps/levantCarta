7ac563a4da4ad68918553cc3f9de25b995f5cea2
Davi Teodoro <davi.a.teodoro@gmail.com>
2019-11-28 20:40:51 -0300

    adds logic to fetch features from layer only if its not already loaded.

--- store/features.js
+++ store/features.js
@@ -13,7 +13,7 @@ export const mutations = {
     SET_FEATURES(state, features) {
         state.features.push(features)
     },
-    SET_CURRENT_FEATURES(state, features) {
+    UPDATE_CURRENT_FEATURES(state, features) {
         state.currentFeatures = features
     },
     LOAD_LAYER(state, layer) {
@@ -24,14 +24,29 @@ export const mutations = {
 export const actions = {
     async setFeaturesFromLayer({ commit, state, rootState }, layerId) {
 
-        if (state.loadedLayers.includes(layerId)) return;
+        let featureCollection = {}
 
-        const { data } = await axios.get('http://beirut.georio.levantcarta.org/api/v1/get/features/' + layerId);
+        if (state.loadedLayers.includes(layerId)) {
+            featureCollection = state.features.filter(layer => {
+                console.log(layer)
+                console.log(layerId)
+                return layer.layerId === layerId
+            })[0]
+            commit('UPDATE_CURRENT_FEATURES', featureCollection)
+
+            console.log('NO API', featureCollection)
+        } else {
+            const { data } = await axios.get('http://beirut.georio.levantcarta.org/api/v1/get/features/' + layerId);
+            console.log('API')
+            featureCollection = data
+            commit('UPDATE_CURRENT_FEATURES', { ...data, layerId })
+            commit('SET_FEATURES', { ...data, layerId })
+            commit('LOAD_LAYER', layerId)
+        }
 
 
-        rootState.draw.add(data)
-
-        commit('LOAD_LAYER', layerId)
+        rootState.draw.deleteAll()
+        rootState.draw.add(featureCollection)
 
     }
 }
