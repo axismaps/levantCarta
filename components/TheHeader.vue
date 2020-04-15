e31a9c33a93249b84b2345123f53f641d28528ca
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-04-15 16:39:01 -0300

    adds control btns

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,45 +1,58 @@
 <template>
   <div class="header">
-    <el-select
-      id="layer-dropdown"
-      @change="handleLayerSelect"
-      v-model="activeLayer"
-      placeholder="Select"
-      :disabled="isEditionInProgress"
-    >
-      <i slot="prefix" class="el-input__icon el-icon-document-copy"></i>
-      <el-option v-for="item in layers" :key="item.id" :label="item.title" :value="item.id"></el-option>
-    </el-select>
-
-    <div class="connection">
-      <i class="el-icon-minus"></i>
-      <i class="el-icon-connection"></i>
-      <i class="el-icon-minus"></i>
-    </div>
-    <el-date-picker
-      id="year-stepper"
-      v-model="layerYear"
-      type="year"
-      format="yyyy"
-      value-format="yyyy"
-      placeholder="Pick a year"
-      @change="handleSetYearCurrentYear"
-    ></el-date-picker>
-    <div class="form-input-with-ctrl-btn">
+    <div class="section">
       <el-select
-        id="basemap-dropdown"
-        @change="handleOverlaySelect"
-        v-model="basemap"
+        id="layer-dropdown"
+        @change="handleLayerSelect"
+        v-model="activeLayer"
         placeholder="Select"
+        :disabled="isEditionInProgress"
       >
-        <el-option v-for="item in overlays" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        <i slot="prefix" class="el-input__icon el-icon-document-copy"></i>
+        <el-option v-for="item in layers" :key="item.id" :label="item.title" :value="item.id"></el-option>
       </el-select>
-      <el-button icon="el-icon-view" type="info" plain></el-button>
-    </div>
 
-    <div id="opacity-slider" class="layerOpacityController">
-      <p>{{layerOpacity}}%</p>
-      <el-slider v-model="layerOpacity" @change="handleSetOverlayOpacity"></el-slider>
+      <div class="connection">
+        <i class="el-icon-minus"></i>
+        <i class="el-icon-connection"></i>
+        <i class="el-icon-minus"></i>
+      </div>
+      <el-date-picker
+        id="year-stepper"
+        v-model="layerYear"
+        type="year"
+        format="yyyy"
+        value-format="yyyy"
+        placeholder="Pick a year"
+        @change="handleSetYearCurrentYear"
+      ></el-date-picker>
+      <div class="form-input-with-ctrl-btn">
+        <el-select
+          id="basemap-dropdown"
+          @change="handleOverlaySelect"
+          v-model="basemap"
+          placeholder="Select"
+        >
+          <el-option v-for="item in overlays" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+        <el-button icon="el-icon-view" type="info" plain></el-button>
+      </div>
+
+      <div id="opacity-slider" class="layerOpacityController">
+        <p>{{layerOpacity}}%</p>
+        <el-slider v-model="layerOpacity" @change="handleSetOverlayOpacity"></el-slider>
+      </div>
+    </div>
+    <div class="section">
+      <el-button class="font-awesome-icon" type="text" circle>
+        <font-awesome-icon :icon="['far', 'cog']" />
+      </el-button>
+      <el-button class="font-awesome-icon" type="text" circle>
+        <font-awesome-icon :icon="['far', 'bell']" />
+      </el-button>
+      <el-button class="font-awesome-icon" type="text" circle>
+        <font-awesome-icon :icon="['far', 'user-circle']" />
+      </el-button>
     </div>
   </div>
 </template>
@@ -85,7 +98,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .header {
   min-height: $header-height;
   width: 100%;
@@ -95,7 +108,11 @@ export default {
   box-shadow: 0px 1px 3px rgb(206, 206, 206);
   align-items: center;
   padding: 0px 20px;
+  justify-content: space-between;
 
+  .section {
+    display: flex;
+  }
   .form-input-with-ctrl-btn {
     padding-left: 30px;
   }
@@ -106,6 +123,14 @@ export default {
 
   .connection {
     margin: 10px;
+  }
+
+  .font-awesome-icon {
+    color: #4a5568;
+    font-size: 17px;
+    &:hover {
+      color: $--color-primary;
+    }
   }
 
   .layerOpacityController {
