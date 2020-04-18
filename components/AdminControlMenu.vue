7a2111af33c80c585a671eeab4b692ef09490288
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-04-17 23:53:41 -0300

    adds adminControlMenu component

---
+++ components/AdminControlMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <el-button icon="el-icon-arrow-left">Back</el-button>
+    <div>
+      <el-button type="success" icon="el-icon-check">Close Change Set</el-button>
+      <el-button icon="el-icon-refresh-left">Revert Selected</el-button>
+      <el-button icon="el-icon-refresh-left">Revert</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
