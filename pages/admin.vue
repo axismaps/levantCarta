290421cdb58f43b5a7f7e1e35d3375a755f4fbe1
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-04-17 23:56:58 -0300

    adds /admin route

---
+++ pages/admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <admin-header />
+    <el-row type="flex" justify="center">
+      <el-col :span="14">
+        <nuxt-child />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import AdminHeader from '@/components/AdminHeader';
+export default {
+  components: {
+    AdminHeader
+  }
+};
+</script>
+
+<style scoped>
+.el-row {
+  padding-top: 42px;
+}
+</style>
