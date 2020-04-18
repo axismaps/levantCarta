329935e018b80956f425c8496460543c4bbfd805
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-04-17 23:56:32 -0300

    cleans up default layout css

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,23 +5,13 @@
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+.font-awesome-icon {
+  color: #4a5568;
   font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-
-}
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
 }
 
+.font-awesome-icon:hover {
+  color: #4299e1;
+  color: var(--color-primary);
+}
 </style>
